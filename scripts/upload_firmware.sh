ba1105979295ba7b9fbd2d233cab6ebe7dd4e825
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2026-02-22 15:07:28 +0100

    feat: transition uploaded config files to scaleway

--- scripts/upload_firmware.sh
+++ scripts/upload_firmware.sh
@@ -2,4 +2,4 @@
 
 S3_ASSETS_BUCKET="${1}"
 
-s3cmd --access_key=${GCS_ACCESS_KEY_ID} --secret_key="${GCS_SECRET_ACCESS_KEY}" --host="https://storage.googleapis.com" --host-bucket="https://storage.googleapis.com" sync firmware/ s3://${S3_ASSETS_BUCKET}/
+s3cmd --access_key=${SCW_ACCESS_KEY} --secret_key="${SCW_SECRET_KEY}" --host="https://storage.googleapis.com" --host-bucket="https://storage.googleapis.com" sync firmware/ s3://${S3_ASSETS_BUCKET}/
